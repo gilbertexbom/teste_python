ba5c6c6327c699d700fedb0cd5ef42c29bb4a5c5
Lumanunes <lumapnunes48@gmail.com>
2024-06-11 11:32:55 -0300

    Commit

--- calculadora.py
+++ calculadora.py
@@ -8,7 +8,8 @@ while True:
     print("2. Subtração")
     print("3. Multiplicação") 
     print("4. Divisão")
-    print("5. Sair")
+    print("5. Modulo")
+    print("6. Sair")
     
 
     op = int(input("\nOpção: "))
@@ -45,11 +46,8 @@ while True:
         print("\n\t\t{} x {} = {}\n" .format(n1,n2,total))
 
     elif op == 4:
-<<<<<<< HEAD
         print("\n\t\t -- Divisão -- \n")
-=======
-        print("\n\"\t\t\t -- Divisão -- \n")
->>>>>>> f8bf5cb2b11978d7b40ece69872b77a716a03c08
+
         
         n1 = float(input("Informe N1: "))
         n2 = float(input("Informe N2: "))
@@ -57,8 +55,20 @@ while True:
         total = n1/n2
         
         print("\n\t\t{} / {} = {}\n".format(n1, n2, total))
-
+        
     elif op == 5:
+        print("\n\t\t -- Modulo -- \n")
+        
+        n1 = float(input("Informe N1: "))
+        n2 = float(input("Informe N2: "))
+        
+        total = n1 % n2
+        
+        print("\n\t\t{} % {} = {}\n".format(n1,n2,total))
+
+        
+
+    elif op == 6:
         print("\n\nAbraço!!\n\n")
         break
     else:
